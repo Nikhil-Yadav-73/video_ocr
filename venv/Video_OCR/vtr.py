c143b76717b5da2c4631d411f9cd29a5271dfaa3
Nikhil-Yadav-73 <nikhilyadavwork73@gmail.com>
2024-12-13 21:25:20 +0530

    dot

--- venv/Video_OCR/vtr.py
+++ venv/Video_OCR/vtr.py
@@ -5,7 +5,7 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def extract_text_from_video(video_path, output_file=None):
-    # Debugging statements
+    # inki wajah se speed slow hai, optimize karenge baad mein
     print(f"Absolute path: {os.path.abspath(video_path)}")
     print(f"File exists: {os.path.exists(video_path)}")
 
@@ -29,10 +29,9 @@ def extract_text_from_video(video_path, output_file=None):
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
 
-        # Save frame for debugging (optional)
+        # Save karna hai to, every frame
         # cv2.imwrite(f'processed_frame_{frame_count}.jpg', thresh)
 
-        # Perform OCR
         config = '--psm 6 -c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
         text = pytesseract.image_to_string(thresh, config=config).strip()
         
